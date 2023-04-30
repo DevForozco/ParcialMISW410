fdc01664d7432d74fb097665a5a225aafdc40311
Fabian Orozco <f.orozcob@uniandes.edu.co>
2023-04-30 08:30:34 -0500

    Test: Implements a unit test

--- parcial/src/app/componentes/plantas/listar/listar.component.spec.ts
+++ parcial/src/app/componentes/plantas/listar/listar.component.spec.ts
@@ -1,13 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListarComponent } from './listar.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { faker } from '@faker-js/faker';
+import { Planta } from 'src/app/modelos/planta';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ListarComponent', () => {
   let component: ListarComponent;
   let fixture: ComponentFixture<ListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ ListarComponent ]
     })
     .compileComponents();
@@ -15,9 +23,37 @@ describe('ListarComponent', () => {
     fixture = TestBed.createComponent(ListarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should list plants', () => {
+    //Arrange
+    let listPlants: Planta[] = new Array();
+    let expectedSize = 4;
+    Array.from({ length: 3 }).forEach(() => {
+      listPlants.push(createRandomPlant());
+    });
+    //Act
+    component.plantas = listPlants;
+    fixture.detectChanges();
+    //Assert
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(expectedSize);
+  });
 });
+
+export function createRandomPlant(): Planta {
+  return {
+    id: 1,
+    nombre_comun: faker.internet.userName(),
+    nombre_cientifico: faker.internet.userName(),
+    tipo: faker.internet.userName(),
+    altura_maxima: 120,
+    clima: faker.animal.bird.name,
+    sustrato_siembra: faker.address.country.name
+  };
+}
+
